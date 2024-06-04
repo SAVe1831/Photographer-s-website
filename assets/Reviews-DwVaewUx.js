bc20870ec39b40278d5be200ea11eb88436be7c7
SAVe1831 <save1831537@gmail.com>
2024-06-04 21:29:05 +0300

    Updates

---
+++ assets/Reviews-DwVaewUx.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,o,b as t}from"./index-JwTJDXZH.js";const c={},_={class:"w-11 mx-auto"},n=t("h1",null,"Скоро здесь появятся отзывы",-1),a=[n];function r(d,i){return o(),s("div",_,a)}const f=e(c,[["render",r]]);export{f as default};
