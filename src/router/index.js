bd2123aa269f4904a04382bd140347febe0d0ad7
SAVe1831 <save1831537@gmail.com>
2024-06-04 21:09:09 +0300

    6 commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
   scrollBehavior(to, from, savedPosition) {
     return { top: 0 };
   },
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
