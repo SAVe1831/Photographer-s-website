2435a9411aad7a394f95f47b6881a607d3bb37ce
SAVe1831 <save1831537@gmail.com>
2024-06-04 21:09:30 +0300

    Updates

---
+++ assets/Reviews-Bi_tX1xY.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o,b as s}from"./index-D8WGopiz.js";const t={},n=s("h1",null,"Отзывы",-1),_=[n];function r(a,l){return o(),c("div",null,_)}const i=e(t,[["render",r]]);export{i as default};
